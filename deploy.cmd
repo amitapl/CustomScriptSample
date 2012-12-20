73307f5c72d18d081d7f2f8dcd4ed54aab160d79
Amit Apple <amitap@microsoft.com>
2012-12-20 14:10:04 -0800

    Fix deploy.cmd script.

--- deploy.cmd
+++ deploy.cmd
@@ -1,10 +1,10 @@
 @echo off
 
 IF "%SITE_FLAVOR%" == "nodejs" (
-  echo deploy.node.cmd
+  deploy.node.cmd
 ) ELSE (
   IF "%SITE_FLAVOR%" == "mvc4" (
-    echo deploy.mvc4.cmd
+    deploy.mvc4.cmd
   ) ELSE (
     echo You have to set SITE_FLAVOR setting to either "nodejs" or "mvc4"
     exit /b 1
